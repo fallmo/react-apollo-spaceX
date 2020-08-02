7c6cf0adb9beb8721246fc8a17f2914d12d979b7
Mohamed Fall <ps.hackmaster@gmail.com>
2020-08-02 08:34:18 +0100

    first dev commit

---
+++ server/README.md
@@ -0,0 +1,23 @@
+# node-typescript-starter
+
+Basic Express Server with Typescript
+
+## Scripts
+
+- _start_: run compiled app '/dist/app.js'
+- _dev_: watch dev server '/src/app.ts' with nodemon
+- _build_: compile typescript app 'src/app.ts' to 'dist' folder
+
+## Usage
+
+1. Clone repo
+2. Edit src/app.ts
+3. Run Build Command
+
+## devDependancies
+
+- @types/express
+- @types/node
+- nodemon
+- ts-node
+- typescript
---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
+
+const app: Application = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
